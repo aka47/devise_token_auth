ed7abcc9922f6e5ee21210a73e3d87290f266afb
Lion Vollnhals <lion@giantmonkey.de>
2015-08-23 17:00:34 +0200

    before_action -> before_filter

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -2,7 +2,7 @@
 module DeviseTokenAuth
   class SessionsController < DeviseTokenAuth::ApplicationController
     before_filter :set_user_by_token, :only => [:destroy]
-    after_action :reset_session, :only => [:destroy]
+    after_filter :reset_session, :only => [:destroy]
 
     def new
       render json: {
